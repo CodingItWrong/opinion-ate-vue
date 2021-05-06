db4ad141868d060a7deec6dd242e6a2a4279fa48
Josh Justice <me@codingitwrong.com>
2021-05-06 07:33:26 -0400

    Add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="app">
-    <RestaurantScreen />
-  </div>
+  <v-app>
+    <v-app-bar color="primary" fixed app>
+      <v-toolbar-title>Opinion Ate</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <RestaurantScreen />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
