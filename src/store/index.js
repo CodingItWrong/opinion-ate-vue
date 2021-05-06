33129cec949cdbea420a07dd969382e286f91431
Josh Justice <me@codingitwrong.com>
2021-05-06 07:01:33 -0400

    Build API call

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import restaurants from './restaurants';
+import api from '@/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {restaurants},
+  modules: {
+    restaurants: restaurants(api),
+  },
 });
