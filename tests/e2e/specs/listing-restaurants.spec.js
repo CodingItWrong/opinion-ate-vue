e88c154ee3762499bcea2ba7b9ebac2b0050f96b
Josh Justice <me@codingitwrong.com>
2021-05-06 06:40:11 -0400

    Specify app should list restaurants

---
+++ tests/e2e/specs/listing-restaurants.spec.js
@@ -0,0 +1,22 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url:
+        'https://outside-in-dev-api.herokuapp.com/89v11ndLhlo4NBm9p6MWuybIx1hjJMF8/restaurants',
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
