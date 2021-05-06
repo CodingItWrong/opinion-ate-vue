360dcd1cc6b86e9ec6865a20558b42bc33b99ee0
Josh Justice <me@codingitwrong.com>
2021-05-06 07:32:39 -0400

    Add Vuetify

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://outside-in-dev-api.herokuapp.com/89v11ndLhlo4NBm9p6MWuybIx1hjJMF8',
+  baseURL:
+    'https://outside-in-dev-api.herokuapp.com/89v11ndLhlo4NBm9p6MWuybIx1hjJMF8',
 });
 
 const api = {
