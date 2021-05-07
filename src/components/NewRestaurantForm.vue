b20e08fc59b445247b1c67455cf5a0ca5e8a05f2
Josh Justice <me@codingitwrong.com>
2021-05-07 06:41:36 -0400

    Dispatch action upon clicking add

--- src/components/NewRestaurantForm.vue
+++ src/components/NewRestaurantForm.vue
@@ -1,10 +1,40 @@
 <template>
-  <form>
-    <v-text-field placeholder="Add Restaurant" filled type="text" />
-    <v-btn color="primary" class="black--text">Add</v-btn>
+  <form @submit.prevent="handleSave">
+    <v-text-field
+      placeholder="Add Restaurant"
+      filled
+      type="text"
+      v-model="name"
+      data-testid="new-restaurant-name-field"
+    />
+    <v-btn
+      type="submit"
+      color="primary"
+      class="black--text"
+      data-testid="new-restaurant-submit-button"
+    >
+      Add
+    </v-btn>
   </form>
 </template>
 
 <script>
-export default {name: 'NewRestaurantForm'};
+import {mapActions} from 'vuex';
+
+export default {
+  name: 'NewRestaurantForm',
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    ...mapActions({
+      createRestaurant: 'restaurants/create',
+    }),
+    handleSave() {
+      this.createRestaurant(this.name);
+    },
+  },
+};
 </script>
---
+++ tests/unit/components/NewRestaurantForm.spec.js
@@ -0,0 +1,63 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+import {mount, createLocalVue} from '@vue/test-utils';
+import NewRestaurantForm from '@/components/NewRestaurantForm.vue';
+
+Vue.use(Vuetify);
+
+describe('NewRestaurantForm', () => {
+  const restaurantName = 'Sushi Place';
+
+  const vuetify = new Vuetify();
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  let restaurantsModule;
+  let wrapper;
+
+  beforeEach(() => {
+    restaurantsModule = {
+      namespaced: true,
+      actions: {
+        create: jest.fn().mockName('create'),
+      },
+    };
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    wrapper = mount(NewRestaurantForm, {
+      localVue,
+      store,
+      vuetify,
+      attachTo: div,
+    });
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('when filled in', () => {
+    beforeEach(() => {
+      wrapper
+        .find('[data-testid="new-restaurant-name-field"]')
+        .setValue(restaurantName);
+      wrapper
+        .find('[data-testid="new-restaurant-submit-button"]')
+        .trigger('click');
+    });
+
+    it('dispatches the create action', () => {
+      expect(restaurantsModule.actions.create).toHaveBeenCalledWith(
+        expect.anything(),
+        restaurantName,
+      );
+    });
+  });
+});
