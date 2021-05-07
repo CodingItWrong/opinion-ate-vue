dfcc23dd182702b945d4e1aa83eb56e5377de3ae
Josh Justice <me@codingitwrong.com>
2021-05-07 06:35:40 -0400

    Drive out UI from E2E test

---
+++ src/components/NewRestaurantForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <form>
+    <v-text-field placeholder="Add Restaurant" filled type="text" />
+    <v-btn color="primary" class="black--text">Add</v-btn>
+  </form>
+</template>
+
+<script>
+export default {name: 'NewRestaurantForm'};
+</script>
